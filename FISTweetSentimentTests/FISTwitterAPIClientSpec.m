7a8ae970dbfbae0331c5816079f66706d4c2e53f
JimCampagno <jimcampagno@gmail.com>
2015-09-10 15:56:23 -0400

    create test file

---
+++ FISTweetSentimentTests/FISTwitterAPIClientSpec.m
@@ -0,0 +1,42 @@
+//
+//  FISTwitterAPIClientSpec.m
+//  FISTweetSentiment
+//
+//  Created by James Campagno on 9/10/15.
+//  Copyright 2015 James Campagno. All rights reserved.
+//
+
+#import "Specta.h"
+#import "FISTwitterAPIClient.h"
+#import "FISSentiment140API.h"
+#import "FISConstants.h"
+
+
+SpecBegin(FISTwitterAPIClient)
+
+describe(@"FISTwitterAPIClient", ^{
+    
+    beforeAll(^{
+        
+        
+
+    });
+    
+    beforeEach(^{
+
+    });
+    
+    it(@"", ^{
+
+    });  
+    
+    afterEach(^{
+
+    });
+    
+    afterAll(^{
+
+    });
+});
+
+SpecEnd
